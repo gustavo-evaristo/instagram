4f1bbf3fc5e07bdd267f1eb6b500bae4d6d28c84
gustavo-evaristo <gug.henri1@gmail.com>
2021-06-14 23:43:53 -0300

    add changes

--- src/server.js
+++ src/server.js
@@ -4,18 +4,9 @@ const mongoose = require('mongoose')
 const path = require('path')
 const cors = require('cors')
 
-
 //Iniciando o app
 const app = express()
 const server = require('http').Server(app)
-
-//Socket io
-const io = require('socket.io')(server)
-app.use((req, res, next) => {
-    req.io = io
-    
-    next()
-})
 
 //Conexão com o cluster do MongoDB Atlas 
 mongoose.connect("mongodb+srv://root:123@senac@cluster0.uijtr.mongodb.net/<dbname>?retryWrites=true&w=majority", {
@@ -25,6 +16,16 @@ mongoose.connect("mongodb+srv://root:123@senac@cluster0.uijtr.mongodb.net/<dbnam
 
 //Habilitando o acesso da API
 app.use(cors())
+
+app.use(express.json())
+
+//Socket io
+const io = require('socket.io')(server)
+app.use((req, res, next) => {
+    req.io = io
+    
+    next()
+})
 
 //Imagens estaticas
 app.use('/files', express.static(path.join(__dirname, '..', 'uploads', 'resized')))
